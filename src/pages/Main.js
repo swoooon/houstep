6c998eeeb60daf456dcf09b67892a962e5ca6d03
Seo BeomSeok <soulstair@gmail.com>
2021-12-19 14:57:51 +0900

    feat: add router with link tag

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import Layout from '../components/Layout';
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+
+import Layout from '../components/Layout'
 import { ReactComponent as Icon } from '../assets/icon.svg'
 import { ReactComponent as Props } from '../assets/PROPS.svg'
 import KakaoLogin from '../assets/kakaoLogin.png'
@@ -22,17 +24,24 @@ const introStyle = {
 }
 
 const kakaoStyle = {
-  marginTop: "70%"
+  marginTop: "70%",
+  border: "0",
+  outline: "0",
+  backgroundColor: 'transparent'
 }
 
 const Main = () => {
+
+  const kakaoClicked = () => {
+    console.log('kakaoClicked')
+  }
 
   return (
     <Layout>
       <Icon style={ iconStyle }/>
       <Props style={ propsStyle }/>
       <div style={ introStyle }> 안전한 계약을 위한 첫걸음 </div>
-      <img style={ kakaoStyle } src={ KakaoLogin } alt='kakaoLogin'/>
+      <button style={ kakaoStyle } type='button' onClick={()=>kakaoClicked()}> <Link to='/'> <img src={KakaoLogin} alt='kakaoLogin'/> </Link> </button>
     </Layout>
   );
 };
