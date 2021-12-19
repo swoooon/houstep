176daf196a3b263a21532cd3e0653d1fedc168fd
Seo BeomSeok <soulstair@gmail.com>
2021-12-19 17:26:12 +0900

    feat: add search page

--- src/components/Root.js
+++ src/components/Root.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from "../pages/Home";
 import Main from "../pages/Main";
+import Search from "../pages/Search";
 
 const Root = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={Home()} />
       <Route path="/main" element={Main()} />
+      <Route path="/search" element={Search()} />
     </Routes>
   </BrowserRouter>
 )
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,26 +2,10 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 
 import Layout from '../components/Layout'
+import styles from '../assets/Icon.module.scss'
 import { ReactComponent as Icon } from '../assets/icon.svg'
 import { ReactComponent as Props } from '../assets/PROPS.svg'
 import KakaoLogin from '../assets/kakaoLogin.png'
-
-const iconStyle = {
-  width: "100%",
-  marginTop: "30%"
-};
-
-const propsStyle = {
-  width: "100%",
-  marginTop: "5%"
-};
-
-const introStyle = {
-  width: "100%",
-  marginTop: "10%",
-  fontSize: "0.85em",
-  color: "grey"
-}
 
 const kakaoStyle = {
   marginTop: "70%",
@@ -31,17 +15,12 @@ const kakaoStyle = {
 }
 
 const Main = () => {
-
-  const kakaoClicked = () => {
-    console.log('kakaoClicked')
-  }
-
   return (
     <Layout>
-      <Icon style={ iconStyle }/>
-      <Props style={ propsStyle }/>
-      <div style={ introStyle }> 안전한 계약을 위한 첫걸음 </div>
-      <button style={ kakaoStyle } type='button' onClick={()=>kakaoClicked()}> <Link to='/'> <img src={KakaoLogin} alt='kakaoLogin'/> </Link> </button>
+      <Icon className={styles.icon}/>
+      <Props className={styles.logo}/>
+      <div className={styles.intro}> 안전한 계약을 위한 첫걸음 </div>
+      <button style={kakaoStyle} type='button'> <Link to='/search'> <img src={KakaoLogin} alt='kakaoLogin'/> </Link> </button>
     </Layout>
   );
 };
