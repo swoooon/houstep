5ef684ee9a4dc8508f66dee3c50855b6615d822a
Seo BeomSeok <soulstair@gmail.com>
2021-12-19 20:41:35 +0900

    chore: test search modal page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
-import styles from "../assets/Header.module.scss"
-import { ReactComponent as Logo } from "../assets/logo.svg"
-import { ReactComponent as MenuIcon } from "../assets/menuIcon.svg"
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import styles from '../assets/Header.module.scss'
+import logoStyles from '../assets/Icon.module.scss'
+import Logo from '../assets/logo.svg'
+import { ReactComponent as MenuIcon } from '../assets/menuIcon.svg'
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.contents}>
-        <Logo/>
+        <button className={logoStyles.buttonLogo} type='button'> <Link to='/main'> <img src={Logo} alt='propsLogo'/> </Link> </button>
         <MenuIcon/>
       </div>
     </header>
