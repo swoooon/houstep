9a58b938d0e819f72ddf7f72027581196b92c1e9
beomxtone <beomxtone@github.com>
2022-01-22 16:24:57 +0900

    feat: attention page feature

--- src/pages/Attention.jsx
+++ src/pages/Attention.jsx
@@ -4,10 +4,11 @@ import styles from '../assets/Icon.module.scss'
 import { ReactComponent as Icon } from '../assets/orangeIcon.svg'
 import { ReactComponent as AttentionBar } from '../assets/attentionBar.svg'
 import { ReactComponent as WarningIcon } from '../assets/warningIcon.svg';
+import { ReactComponent as AttentionReport } from '../assets/attentionReport.svg'
 
 const Attention = () => {
   return (
-    <Layout>
+    <Layout style={{backgroundColor: 'blue'}}>
       <Icon style={{ marginTop: '35px', marginLeft: '26px', float: 'left' }}/>
       <br/>
       <AttentionBar style={{ marginTop: '24px' }}/>
@@ -34,8 +35,81 @@ const Attention = () => {
       </div>
       
       {/** Report of Detailed diagnosis */}
-      <div className={styles.box} style={{marginTop: '35px', height: '1492px'}}></div>
+      <div className={styles.box} style={{marginTop: '35px', height: '1410px', padding: '20px'}}>
+        <div style={{marginLeft: '10px', fontSize: '1.1em', fontWeight: '600'}}>세부 진단 리포트</div>
+        <div style={{margin: '15px', textAlign: 'center', fontSize: '1.1em', fontWeight: '600'}}>
+          신축이라 &nbsp;
+          <span style={{background: 'linear-gradient(to top, #F8D892 60%, transparent 50%)'}}>
+            아직 등기부가 없어요!</span>
+        </div>
+        <AttentionReport style={{display: 'block', margin: '0 auto', marginTop: '30px'}}/>
+        <>
+          <div 
+            style={{marginTop: '30px', height: '31px', lineHeight: '31px', backgroundColor: '#F3F3F3', borderRadius: '15px', textAlign: 'center', fontSize: '0.9em', fontWeight: '600'}}>
+              신축이라 아직 등기부등본이 없어요!
+          </div>
+          <div style={{marginTop: '15px', marginLeft: '10px', marginRight: '10px', fontSize: '0.85em', fontWeight: '500'}}>
+            <div>미등기 상태의 매물입니다.</div>
+            <div style={{marginTop: '10px'}}>따라서 분양 계약서, 키불출 확인서와 같은 <br/>서류를 통해 소유자를 확인해야 해요!</div>
+            <div style={{marginTop: '10px'}}>계약 시 소유자(집주인)와 임대인이 같은지도 꼭 확인해주세요.</div>
+          </div>
+        </>
 
+        <>
+          <div 
+            style={{marginTop: '30px', height: '31px', lineHeight: '31px', backgroundColor: '#F3F3F3', borderRadius: '15px', textAlign: 'center', fontSize: '0.9em', fontWeight: '600'}}>
+              특약사항을 꼭 걸어주세요!
+          </div>
+          <div style={{marginTop: '15px', marginLeft: '10px', marginRight: '10px', fontSize: '0.85em', fontWeight: '500'}}>
+            <div>'전세보증금 잔금 전 또는 보증금을 받는 즉시 집주인이 분양 잔금을 완납하지 않으면 전세 <br/> 계약을 해지한다' 라는 내용의 특약사항을 넣으시길 추천드려요.</div>
+          </div>
+        </>
+
+        <>
+          <div 
+            style={{marginTop: '30px', height: '31px', lineHeight: '31px', backgroundColor: '#F3F3F3', borderRadius: '15px', textAlign: 'center', fontSize: '0.9em', fontWeight: '600'}}>
+              임대인 신분증을 꼭 확인해주세요
+          </div>
+          <div style={{marginTop: '15px', marginLeft: '10px', marginRight: '10px', fontSize: '0.85em', fontWeight: '500'}}>
+            <div>공급 계약서나 분양 계약서상 소유자와 현재 <br/> 임대인이 같은 사람인지를 꼭 확인해야 합니다.</div>
+            <div style={{marginTop: '10px'}}>계좌 송금 역시 반드시 집주인 명의 계좌인지 확인하신 후 송금하세요!</div>
+          </div>
+        </>
+
+        <>
+          <div 
+            style={{marginTop: '30px', height: '31px', lineHeight: '31px', backgroundColor: '#F3F3F3', borderRadius: '15px', textAlign: 'center', fontSize: '0.9em', fontWeight: '600'}}>
+              가압류, 가처분이 없는지 확인하세요
+          </div>
+          <div style={{marginTop: '15px', marginLeft: '10px', marginRight: '10px', fontSize: '0.85em', fontWeight: '500'}}>
+            <div>전세보증금을 돌려받을 때 문제가 없도록 분양계약서에 가압류, 가처분이 걸린게 없는지 확인해야 합니다.</div>
+            <div style={{marginTop: '10px'}}>이는 시행사, 시공사에 연락하시면 알아보실 수 있습니다.</div>
+          </div>
+        </>
+
+        <>
+          <div 
+            style={{marginTop: '30px', height: '31px', lineHeight: '31px', backgroundColor: '#F3F3F3', borderRadius: '15px', textAlign: 'center', fontSize: '0.9em', fontWeight: '600'}}>
+              확정일자 발급과 전입신고 잊지마세요!
+          </div>
+          <div style={{marginTop: '15px', marginLeft: '10px', marginRight: '10px', fontSize: '0.85em', fontWeight: '500'}}>
+            <div>미등기아파트라도 확정일자를 받을 수 있어요!</div>
+            <div style={{marginTop: '10px'}}>확정일자를 받지 않으면 새로운 집주인이 나타났을때 임대차 계약이 존속되지 않을 수 있기 때문에 무조건 확정일자와 전입신고를 하심을 추천드립니다.</div>
+          </div>
+        </>
+
+        <>
+          <div 
+            style={{marginTop: '30px', height: '31px', lineHeight: '31px', backgroundColor: '#F3F3F3', borderRadius: '15px', textAlign: 'center', fontSize: '0.9em', fontWeight: '600'}}>
+              전문가와 함께 거래해야해요
+          </div>
+          <div style={{marginTop: '15px', marginLeft: '10px', marginRight: '10px', fontSize: '0.85em', fontWeight: '500'}}>
+            <div>해당 집의 경우 아직 등기가 되지 않은 집이기 때문에 안전을 위해 공인중개사와 계약하시길 추천드립니다.</div>
+            <div style={{marginTop: '10px'}}>공인중개사나 법률전문가분께 해당 상황에 대한 정확한 설명을 들으신 후 안전하게 계약하시길 바랍니다.</div>
+          </div>
+        </>
+      </div>
+      <div style={{height: '80px'}}/>
     </Layout>
   )
 }
