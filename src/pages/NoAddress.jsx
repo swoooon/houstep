b121f0fc6f00b5c5900ed657bb3f9715a9574e3f
beomxtone <beomxtone@github.com>
2022-01-22 15:10:54 +0900

    style: hide scroll bar but using

--- src/pages/NoAddress.jsx
+++ src/pages/NoAddress.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// eslint-disable-next-line no-unused-vars
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 import Layout from '../components/LayoutNoHeader';
 
